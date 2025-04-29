abf6312337164a56ed7e43d37719fa30cb513559
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-04-29 13:19:18 +0200

    refactor: improve entry script

--- bin/tuner.sh
+++ bin/tuner.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-WORKING_DIR=$(pwd)
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "$SCRIPT_DIR" || exit
+SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)
 
 # find JS entrypoint
-tuner=$(node -e "console.log(require.resolve('api-tuner/bin/index.js'))" 2> /dev/null)
-
-cd "$WORKING_DIR" || exit
+tuner="$SCRIPT_DIR/index.js"
 
 # if tsx exists in path
 if command -v tsx > /dev/null 2>&1
