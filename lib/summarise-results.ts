6fab2ba015316c3023f2de86a20cc690fb2c1952
tpluscode <tpluscode@users.noreply.github.com>
2026-07-01 13:56:47 +0200

    feat: add `--grep` option

--- lib/summarise-results.ts
+++ lib/summarise-results.ts
@@ -1,46 +1,44 @@
 import type { Readable } from 'node:stream'
-import SHACLValidator from 'rdf-validate-shacl'
 import rdf from '@zazuko/env-node'
-
-const shapesTtl = new URL('./shapes.ttl', import.meta.url)
 
 interface Result {
   summary: string
   success: boolean
+  allTestsSkipped: boolean
 }
 
 export default async (resultStream: Readable): Promise<Result> => {
-  const shapes = await rdf.dataset().import(rdf.fromFile(shapesTtl))
-  const validator = new SHACLValidator(shapes, {
-    factory: rdf,
-  })
-
   const data = await rdf.dataset().import(rdf.formats.parsers.import('text/n3', resultStream, {
     format: 'n3',
   })!)
-
-  const validationReport = await validator.validate(data)
 
   const testCases = rdf.clownface({ dataset: data })
     .has(rdf.ns.rdf.type, rdf.ns.earl.TestCase)
     .toArray()
 
   const summary = []
+  let success = true
+  let allTestsSkipped = true
   for (const testCase of testCases) {
     const { hash } = new URL(testCase.value)
-    const result = validationReport.results.find(result => testCase.term.equals(result.focusNode))
     const label = testCase.out(rdf.ns.rdfs.label).value
     const resultLine = label ? `${label} (<${hash}>)` : `<${hash}>`
 
-    if (rdf.ns.sh.Violation.equals(result?.severity)) {
-      summary.push(`❌  FAIL   ${resultLine}`)
-    } else {
+    if (testCase.has(rdf.ns.earl.outcome, rdf.ns.earl.passed).values.length) {
       summary.push(`✅  PASS   ${resultLine}`)
+      allTestsSkipped = false
+    } else if (testCase.has(rdf.ns.earl.outcome, rdf.ns.earl.failed).values.length) {
+      summary.push(`❌  FAIL   ${resultLine}`)
+      allTestsSkipped = false
+      success = false
+    } else {
+      summary.push(`⏳  SKIP   ${resultLine}`)
     }
   }
 
   return {
-    success: validationReport.conforms,
+    success,
     summary: summary.join('\n'),
+    allTestsSkipped,
   }
 }
